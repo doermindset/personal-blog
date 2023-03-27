fabc3c853b53050f70a692ad74c454fa85ef9158
doermindset <raul.stefan.pop@stud.ubbcluj.ro>
2023-03-27 21:04:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Blog
+
+Learn the basisc of REST API, NoSQL Database and EJS by creating a personal blog.
---
+++ app.js
@@ -0,0 +1,64 @@
+import env from "dotenv";
+env.config();
+import express from "express";
+import bodyParser from "body-parser";
+import _ from "lodash" ;
+import mongoose from 'mongoose';
+import Post from './model/Post.js';
+
+
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.DB_CONNECTION);
+
+app.get('/', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('home', {posts:posts});
+});
+
+app.get('/posts/:name', async(req, res) => {
+    const posts = await Post.find({});
+    posts.forEach((post) => {
+    let titlePost = _.kebabCase(post.title);
+    if(titlePost === _.kebabCase(req.params.name)) {
+      res.render(('post'), {post:post});
+    }
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {aboutContent:aboutContent});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {contactContent:contactContent});
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', async(req, res) => {
+
+  await Post.create({
+      title:req.body.title,
+      content: req.body.postArea,
+  });
+
+  res.redirect('/');
+  
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
